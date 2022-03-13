11f69f0eb26ff64dc658d66e6888fccebbd9c649
taellinglin <taellinglin@users.noreply.github.com>
2022-03-13 08:22:30 +0000

    Black lint
    Signed-off-by: Black <actions@github.com>

--- simplemidi.py
+++ simplemidi.py
@@ -1,8 +1,9 @@
 from IPython.display import Audio
 from pretty_midi import PrettyMIDI
 
-class simplemidi():
+
+class simplemidi:
     def playmidi(midi_file, sf2_path):
         music = PrettyMIDI(midi_file=midi_file)
-        waveform = music.fluidsynth(fs = 192000000, sf2_path=sf2_path)
+        waveform = music.fluidsynth(fs=192000000, sf2_path=sf2_path)
         Audio(waveform, rate=44100)
