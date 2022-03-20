f38a7bdd53e4a2398682c5e88260b000f9f27578
nopeless <38830903+nopeless@users.noreply.github.com>
2022-03-21 08:17:27 +0900

    add TODO

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -14,6 +14,13 @@ logging.info("loaded logger from config")
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+# TODO
+# Fill out these classes
+
 
 class Font:
     default = pygame.font.Font("freesansbold.ttf", 32)
+
+
+class Color:
+    pass
