c7a5ed725abb850b71bb867e948946677b16fdaf
nopeless <38830903+nopeless@users.noreply.github.com>
2022-03-21 08:17:36 +0900

    engine image manager tile test

--- src/engine/__init__.py
+++ src/engine/__init__.py
@@ -2,3 +2,5 @@ from .managers import AudioManager, ImageManager
 
 audioManager = AudioManager("resources/audio")
 imageManager = ImageManager("resources/images")
+
+imageManager.add_resource("tile", "sprites/test-tile.png")
