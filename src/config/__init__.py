425d4343a30770fe00325c70fa1639c2041f5ec9
nopeless <38830903+nopeless@users.noreply.github.com>
2022-03-21 07:38:49 +0900

    add fonts

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,9 +1,16 @@
 # setup stuff here
 
 import logging
+from enum import Enum
+
+import pygame
 
 logging.basicConfig(filename="game.debug.log", level=logging.DEBUG)
 logging.info("loaded logger from config")
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
+
+class Fonts(Enum):
+    default = pygame.font.Font("freesansbold.ttf", 32)
