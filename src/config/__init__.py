294ec1186f538db816e94a0414e0b7b0c0963550
nopeless <38830903+nopeless@users.noreply.github.com>
2022-03-21 18:29:55 +0900

    remove print

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -14,7 +14,6 @@ def hex_to_rgb(value):
     value = value.lstrip("#")
     lv = len(value)
     rgb = tuple(int(value[i : i + lv // 3], 16) for i in range(0, lv, lv // 3))
-    print(str(rgb[0]) + "," + str(rgb[1]) + "," + str(rgb[2]))
     return rgb
 
 
