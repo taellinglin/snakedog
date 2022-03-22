29d9a55784efe7e801edcd225f14757fb3ab6b0f
nopeless <38830903+nopeless@users.noreply.github.com>
2022-03-23 04:21:13 +0900

    fix file path

--- src/loaders/__init__.py
+++ src/loaders/__init__.py
@@ -3,5 +3,5 @@ from .managers import AudioManager, ImageManager
 audioManager = AudioManager("resources/audio")
 imageManager = ImageManager("resources/images", "fallback.png")
 
-imageManager.add_resource("tile", "sprites/test-tile.png")
+imageManager.add_resource("tile", "sprites/test_tile.png")
 imageManager.add_resource("player", "sprites/player.png")
