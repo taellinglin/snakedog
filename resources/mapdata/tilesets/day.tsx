a7365ffaa6ee59d3c9458f1855e8bfe9e9c90bfc
nopeless <38830903+nopeless@users.noreply.github.com>
2022-03-24 22:26:27 +0900

    restructure resources

---
+++ resources/mapdata/tilesets/day.tsx
@@ -0,0 +1,180 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Day" tilewidth="64" tileheight="64" tilecount="50" columns="10">
+ <image source="day.png" width="640" height="320"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="27" duration="500"/>
+   <frame tileid="17" duration="500"/>
+   <frame tileid="7" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+   <object id="2" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="4">
+   <object id="4" x="0" y="0" width="64" height="64"/>
+   <object id="5" x="0" y="0" width="64" height="64"/>
+   <object id="6" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="41">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <wangsets>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="4" wangid="1,1,0,0,0,1,1,1"/>
+   <wangtile tileid="5" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="10" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="12" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="14" wangid="0,0,0,1,1,1,1,1"/>
+   <wangtile tileid="15" wangid="0,1,1,1,1,1,0,0"/>
+   <wangtile tileid="20" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="21" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="22" wangid="1,0,0,0,0,0,1,1"/>
+  </wangset>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="4" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="5" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="14" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="15" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="7" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="17" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="27" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="40" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="41" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="42" wangid="0,1,0,1,0,1,0,1"/>
+  </wangset>
+  <wangset name="Unnamed Set" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="7" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="10" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="11" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="12" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="17" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="20" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="21" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="22" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="27" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="34" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="40" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="41" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="42" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="46" wangid="1,0,1,0,1,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
