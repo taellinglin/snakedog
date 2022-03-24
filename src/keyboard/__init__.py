275e3d2265202e6c176121766dcd4a9ae7f8a3a5
nopeless <38830903+nopeless@users.noreply.github.com>
2022-03-24 22:26:52 +0900

    fix keys

--- src/keyboard/__init__.py
+++ src/keyboard/__init__.py
@@ -9,13 +9,13 @@ def pygame_event_to_dxdy(e):
         return
     return {
         pygame.K_w: (0, -1),
-        pygame.UP: (0, -1),
+        pygame.K_UP: (0, -1),
         pygame.K_a: (-1, 0),
-        pygame.LEFT: (-1, 0),
+        pygame.K_LEFT: (-1, 0),
         pygame.K_s: (0, 1),
-        pygame.DOWN: (0, 1),
+        pygame.K_DOWN: (0, 1),
         pygame.K_d: (1, 0),
-        pygame.RIGHT: (1, 0),
+        pygame.K_RIGHT: (1, 0),
     }.get(e.key, None)
 
 
